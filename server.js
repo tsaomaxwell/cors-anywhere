e36321fe8c27da346e99305b44e44730ce12106d
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-11-14 14:42:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = [];
-var originWhitelist = ["https://club-resources-embark.s3.amazonaws.com", "https://embark-test.netlify.app/", ""];
+var originWhitelist = ["https://club-resources-embark.s3.amazonaws.com", "https://embark-test.netlify.app/", "", "http://localhost:3000"];
 function parseEnvList(env) {
   if (!env) {
     return [];
